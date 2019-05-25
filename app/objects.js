5c7fae8a5c3d9b54641a6b4643fca1f05f777393
Ofonime Usoro <ofonimeusoro01@gmail.com>
2019-05-25 15:46:49 +0200

    Objects section implemented

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,15 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      return fn.call(obj);
     },
 
     alterObjects : function(constructor, greeting) {
-
+      constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
-
+      return Object.keys(obj).map(key => `${key}: ${obj[key]}` );
     }
   };
 });
