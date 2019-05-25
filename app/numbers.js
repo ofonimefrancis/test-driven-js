d710363cb7d2caf94833ce1017672e5e5e2333bb
Ofonime Usoro <ofonimeusoro01@gmail.com>
2019-05-25 16:22:40 +0200

    number section implemented

--- app/numbers.js
+++ app/numbers.js
@@ -3,15 +3,19 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-
+      return 1 & (num >> (bit - 1));
     },
 
     base10: function(str) {
-      return str.toString(10);
+      return parseInt(str, 2);
     },
 
     convertToBinary: function(num) {
-      return num.toString(2);
+      var arr = [];
+      for (var i = 7; i > -1; i--) {
+        arr.push( num & (1 << i) ? 1 : 0 );
+      }
+      return arr.join('');
     },
 
     multiply: function(a, b) {
