bb594b202d30d7a920935f1a3447cc991dfc4289
Ofonime Usoro <ofonimeusoro01@gmail.com>
2019-05-25 15:18:21 +0200

    Async section implemented

--- app/async.js
+++ app/async.js
@@ -3,11 +3,16 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
+      return new Promise(resolve => resolve(value));
     },
 
     manipulateRemoteData : function(url) {
-
+      let result = [];
+      return fetch(url).then(response => response.json())
+        .then(data => {
+          data.people.forEach(item => result.push(item));
+      });
+      return result.sort();
     }
   };
 });
