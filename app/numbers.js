67665fca7b0ad33b3ccd1eb8dadb42c254808669
Ofonime Usoro <ofonimeusoro01@gmail.com>
2019-05-25 14:49:28 +0200

    Regex implemented

--- app/numbers.js
+++ app/numbers.js
@@ -7,15 +7,32 @@ define(function() {
     },
 
     base10: function(str) {
-
+      return str.toString(10);
     },
 
     convertToBinary: function(num) {
-
+      return num.toString(2);
     },
 
     multiply: function(a, b) {
+      var parsedA = a.toString().split('.');
+      var parsedB = b.toString().split('.');
 
+      var aPrecision = 0;
+      var bPrecision = 0;
+
+      if (parsedA.length > 1) {
+        aPrecision = parsedA[1].length;
+      }
+
+      if (parsedB.length > 1) {
+        bPrecision = parsedB[1].length;
+      }
+
+      var precision = Math.max(aPrecision, bPrecision);
+
+
+      return parseFloat((a * b).toFixed(precision));
     }
   };
 });
--- app/regex.js
+++ app/regex.js
@@ -3,26 +3,30 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+      return (/\d/g).test(str);
     },
 
     containsRepeatingLetter : function(str) {
-
+      return (/([A-Z])\1/ig).test(str);
     },
 
     endsWithVowel : function(str) {
-
+      return (/^.+[aeiou]$/i).test(str);
     },
 
     captureThreeNumbers : function(str) {
-
+      var match = (/(\d{3})/).exec(str);
+      if (match) {
+        return match[1];
+      }
+      return false;
     },
 
     matchesPattern : function(str) {
-
+      return (/^\d{3}-\d{3}-\d{4}$/).test(str);
     },
     isUSD : function(str) {
-
+      return (/^\$\d{1,3}(?:,?\d{3})*(?:\.\d{2})?$/).test(str);
     }
   };
 });
